003b3340536dcc2ab3d7cca1b9141f1d662c8f0d
Nadimul Rahib <nadimulrahib38@gmail.com>
2025-01-07 10:02:51 -0800

    added redux

---
+++ src/app/features/quiz/quizSlice.ts
@@ -0,0 +1,14 @@
+import { createSlice } from "@reduxjs/toolkit";
+
+const initialState = {};
+
+export const quizSlice = createSlice({
+  name: "quiz",
+  initialState,
+  reducers: {},
+});
+
+// Action creators are generated for each case reducer function
+// export const {  } = quizSlice.actions
+
+export default quizSlice.reducer;
